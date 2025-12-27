0280513e2212e4bd42f5200ff6f45fae9d47250b
makk-r <makkawan2553@gmail.com>
2025-12-27 13:15:44 +0700

    update-26.1.2

--- app/main.sh
+++ app/main.sh
@@ -1,26 +1,40 @@
 #!/usr/bin/env bash
 
 
-if [ ! -d /usr/lib/IOI/Hydra/tmp ]; then
-    mkdir /usr/lib/IOI/Hydra/tmp
-fi
+if [ "$EUID" -nez 0 ]; then
+    if [ ! -d /usr/lib/IOI/Hydra/tmp ]; then
+        mkdir /usr/lib/IOI/Hydra/tmp
+    fi
 
-if [ ! -d /usr/lib/IOI/venv ]; then
-    python -m venv /usr/lib/IOI/venv
-fi
+    if [ ! -d /usr/lib/IOI/venv ]; then
+        python -m venv /usr/lib/IOI/venv
+    fi
 
-source /usr/lib/IOI/venv/bin/activate
+    source /usr/lib/IOI/venv/bin/activate
 
-pip install -r /usr/lib/IOI/Hydra/requirements.txt --upgrade --quiet
+    pip install -r /usr/lib/IOI/Hydra/requirements.txt --upgrade --quiet
 
-if [ ! -d /usr/lib/IOI/Hydra/modules ]; then
-    mkdir /usr/lib/IOI/Hydra/modules
-fi
-if [ ! -d /usr/lib/IOI/Hydra/lib ]; then
-    mkdir /usr/lib/IOI/Hydra/lib
-fi
-if [ ! -d /usr/lib/IOI/Hydra/docs ]; then
-    mkdir /usr/lib/IOI/Hydra/docs
+    if [ ! -d /usr/lib/IOI/Hydra/modules ]; then
+        mkdir /usr/lib/IOI/Hydra/modules
+    fi
+    if [ ! -d /usr/lib/IOI/Hydra/lib ]; then
+        mkdir /usr/lib/IOI/Hydra/lib
+    fi
+    if [ ! -d /usr/lib/IOI/Hydra/docs ]; then
+        mkdir /usr/lib/IOI/Hydra/docs
+    fi
+    if [ $1 = "-c" ]; then
+        python /usr/lib/IOI/Hydra/src/clonepgk "${@:2}"
+        exit 0
+    fi
+    if [ $1 = "-r" ]; then
+        python /usr/lib/IOI/Hydra/src/removepgk "${@:2}"
+        exit 0
+    fi
+    if [ $1 = "-u" ]; then
+        echo "not update"
+        exit 0
+    fi
 fi
 
 if [ -z $1 ]; then
@@ -43,23 +57,8 @@ if [ $1 = "-v" ]; then
     exit 0
 fi
 
-if [ $1 = "-c" ]; then
-    python /usr/lib/IOI/Hydra/src/clonepgk "${@:2}"
-    exit 0
-fi
-
-if [ $1 = "-r" ]; then
-    python /usr/lib/IOI/Hydra/src/removepgk "${@:2}"
-    exit 0
-fi
-
 if [ $1 = "-l" ]; then
     ls /usr/lib/IOI/Hydra/modules
-    exit 0
-fi
-
-if [ $1 = "-u" ]; then
-    echo "not update"
     exit 0
 fi
 
